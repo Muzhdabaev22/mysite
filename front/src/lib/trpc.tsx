5827eb9c88c58cdd922278b232f2f6b999fbb1ec
Edem <edik622mujpc@gmail.com>
2025-04-01 21:22:53 +0300

    add backend and trpc

--- front/src/lib/trpc.tsx
+++ front/src/lib/trpc.tsx
@@ -1,4 +1,31 @@
-import { createTRPCReact } from "@trpc/react-query";
-import type { TrpcRouter } from '@ideanick/backend/src/trpc'
+import { createTRPCReact } from '@trpc/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { httpBatchLink } from '@trpc/client';
+import type { TrpcRouter } from '@mysite/backend/src/trpc';
 
-const trpc = createTRPCReact<TrpcTouter>{}
+export const trpc = createTRPCReact<TrpcRouter>();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+});
+
+export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
